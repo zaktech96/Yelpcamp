9d32122942482fc2c3ea107a3e56ba7c08a7345e
zaktech96 <96632945+zaktech96@users.noreply.github.com>
2022-07-23 00:18:11 +0100

    updated changes

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../src/models/user');
+const users = require('../controllers/users');
+
+const res = require('express/lib/response');
+
+router.route('/register')
+.get(users.renderRegister)
+.post(catchAsync(users.register))
+
+
+router.route('/login')
+.get(users.renderLogin)
+.post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
+router.get('/logout', users.logout)
+
+
+module.exports = router;
